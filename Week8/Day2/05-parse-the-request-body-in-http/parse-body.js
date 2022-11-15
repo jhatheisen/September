8cb4f0dc423e10719c1756338323495563447b1c
Jhass Theisen <jhasstheisen1@gmail.com>
2022-11-15 14:20:38 -0800

    finished parsing request body

--- Week8/Day2/05-parse-the-request-body-in-http/parse-body.js
+++ Week8/Day2/05-parse-the-request-body-in-http/parse-body.js
@@ -1,25 +1,39 @@
 function firstStep(input) {
   // Your code here
+  return input.split('&');
 }
 
 function secondStep(input) {
   // Your code here
+  return input.map(keyValuePair => keyValuePair.split('='));
 }
 
 function thirdStep(input) {
   // Your code here
+  return input.map(([key, value]) => [key, value.replace(/\+/, ' ')]);
 }
 
 function fourthStep(input) {
   // Your code here
+  return input.map(([key, value]) => [key, decodeURIComponent(value)]);
 }
 
 function fifthStep(input) {
   // Your code here
+  return input.reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc
+  }, {});
 }
 
 function parseBody(str) {
   // Your code here
+  str = firstStep(str);
+  str = secondStep(str);
+  str = thirdStep(str);
+  str = fourthStep(str);
+  str = fifthStep(str);
+  return str;
 }
 
 /******************************************************************************/
--- Week8/Day2/05-parse-the-request-body-in-http/server.js
+++ Week8/Day2/05-parse-the-request-body-in-http/server.js
@@ -4,8 +4,38 @@ let server;
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
+const http = require('http');
 
-// Your code here
+server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+
+  let reqBody = '';
+  req.on('data', data => {
+    reqBody += data;
+  });
+  req.on('end', () => {
+    console.log(reqBody);
+    if (reqBody) {
+      reqBody = reqBody.split('&')
+        .map(keyValuePair => keyValuePair.split('='))
+        .map(([key, value]) => [key, value.replace(/\+/, ' ')])
+        .map(([key, value]) => [key, decodeURIComponent(value)])
+        .reduce((acc, [key, value]) => {
+          acc[key] = value;
+          return acc
+        }, {});
+      req.body = reqBody;
+    }
+    sendFormPage(req, res);
+
+
+  });
+
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Successfully started the server on port ', port));
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
