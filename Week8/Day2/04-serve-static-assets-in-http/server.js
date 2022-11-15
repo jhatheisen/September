7403402e91ebd18ff8d6c2223b1af6b4902fe075
Jhass Theisen <jhasstheisen1@gmail.com>
2022-11-15 14:50:46 -0800

    static using switch case

--- Week8/Day2/04-serve-static-assets-in-http/server.js
+++ Week8/Day2/04-serve-static-assets-in-http/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  
+
   let path = req.url;
   // check if static in url
   if (path.slice(0, 7) === '/static') {
@@ -10,12 +10,22 @@ const server = http.createServer((req, res) => {
     try {
       // add assets to front
       path = './assets' + path.slice(7);
+
       // get file extention
       let extention = path.slice(path.length - 4);
 
-      // if file extention jpg or css change to content type
-      if (extention === '.jpg') extention = 'image/jpeg';
-      if (extention === '.css') extention = 'text/css';
+      // if file extention jpg or css change to content type value
+      switch(extention) {
+        case '.jpg':
+          extention = 'image/jpeg';
+          break;
+        case '.css':
+          extention = 'text/css';
+          break;
+        default :
+          extention = 'text/plain';
+          break;
+      }
 
       // read file path
       const location = fs.readFileSync(path);
@@ -24,6 +34,7 @@ const server = http.createServer((req, res) => {
       res.setHeader('Content-Type', extention);
       // send file
       return res.end(location);
+
       // if file not found log can't find
     } catch (e) {
       console.log('404 Location not found for requested file');
