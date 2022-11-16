5c38efdd2dc64c6416e47a4fba1846c9b7ffbd0c
Jhass Theisen <jhasstheisen1@gmail.com>
2022-11-16 14:35:44 -0800

    finished long practice

--- Week8/Day3/06-fetch-long-practice/fetch-requests.js
+++ Week8/Day3/06-fetch-long-practice/fetch-requests.js
@@ -5,16 +5,60 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here
-
+// fetch post to products
+fetch('/products', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+});
 
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here
+// fetch post and print status and others
+fetch('/products', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+}).then(res => {
+  console.log('status code', res.status);
+  console.log('Content-Type header val: ', res.headers.get('Content-Type'));
+  console.log('url of res', res.url);
+  console.log('redirect?', res.redirected);
+});
 
 
 
 /* ============================== Phase 3 ============================== */
 
 // Your code here
+let body = new URLSearchParams({
+  name: "Caribbean Delight Coffee",
+  description: "Made by Manatee Coffee",
+  price: 11.99,
+  categories: "grocery"
+});
+
+fetch('/products', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+  body: body
+});
+
+
+// 4
+
+let body = new URLSearchParams({
+  name: "Razer Black Widow",
+  description: "RGB Gaming Keyboard",
+  price: 100.00,
+  categories: "electronics"
+});
+
+fetch('/products', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+  body: body
+});
+
+// 4.1 make best selling
