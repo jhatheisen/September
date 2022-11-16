d100a9246883ffede8c1af32f179eff5eba4c001
Jhass Theisen <jhasstheisen1@gmail.com>
2022-11-16 13:38:51 -0800

    finished fetch requests

--- Week8/Day3/04-fetch/fetch-requests.js
+++ Week8/Day3/04-fetch/fetch-requests.js
@@ -7,20 +7,45 @@ Paste your code for fetch requests here once you finish each task.
 
 // Your code here
 
+fetch('/products')
+  .then((res) => {
+    console.log('response status', res.status);
+  });
+
+fetch('/products')
+  .then(res => res.status)
+  .then(console.log);
+
 
 
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
-
+fetch('/products')
+  .then((res) => {
+    console.log('response ok', res.ok);
+  });
 
 
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
-
-
+fetch('/products')
+.then((res) => {
+    console.log('response content type ', res.headers.get('Content-Type'));
+  });
 
 /* ============== 4. Print the body of the response as text =============== */
 
 // Your code here
+fetch('/products')
+.then(res => res.text())
+.then(console.log);
+
+async function getBody(url) {
+  const res = await fetch(url);
+  const text = await res.text();
+  console.log(text);
+}
+
+getBody('/products');
