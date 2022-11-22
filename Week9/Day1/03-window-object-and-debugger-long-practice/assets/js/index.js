80d2fa7f16fde7835b15f6693c913c7844b9f68c
Jhass Theisen <jhasstheisen1@gmail.com>
2022-11-21 16:16:11 -0800

    finished long practice

--- Week9/Day1/03-window-object-and-debugger-long-practice/assets/js/index.js
+++ Week9/Day1/03-window-object-and-debugger-long-practice/assets/js/index.js
@@ -1,8 +1,16 @@
+import { changeTitle, changeHeader, changeAboutMe } from './manipulate.js';
+import * as Find from './search.js';
+
 changeTitle();
 changeHeader();
 changeAboutMe();
-console.log(findElementById("header")); // <div id="header">
-console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
-console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
-console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
-console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+console.log(Find.findElementById("header"));
+// <div id="header">
+console.log(Find.findFirstElementOfTag("LI"));
+// <li><h3>Computer Science</h3>...</li>
+console.log(Find.findFirstElementOfClass("section"));
+// <div class="section"><h2>About Me</h2>...</div>
+console.log(Find.findElementsOfTag("H3"));
+// [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
+console.log(Find.findElementsOfClass("section"));
+// [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
--- Week9/Day1/03-window-object-and-debugger-long-practice/assets/js/manipulate.js
+++ Week9/Day1/03-window-object-and-debugger-long-practice/assets/js/manipulate.js
@@ -1,18 +1,18 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
-    // Your code here
+    window.document.title = "Jhass' Portfolio";
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
-    // Your code here
+    document.body.children[0].children[0].innerText = 'I am Jhass Theisen'
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
 
-    // Your code here
+    document.body.children[1].children[1].innerText = 'I am a student of app academy studying to become a full stack software engineer. I am learning HTML, CSS, JS, and many other skills needed for software development.'
 }
