d1df3bd96c3e57b2953e42c67c39a0d48ad85138
Jhass Theisen <jhasstheisen1@gmail.com>
2022-11-23 10:48:34 -0800

    finished events and added phase 2 file

--- Week9/Day3/02-data-on-elements/data.js
+++ Week9/Day3/02-data-on-elements/data.js
@@ -1,1 +1,51 @@
 // Your code here
+window.addEventListener('DOMContentLoaded', event => {
+
+  const addButton = document.body.querySelector('#add');
+  addButton.addEventListener('click', event => {
+    // stop default submit
+    event.preventDefault();
+    // create list el
+    const li = document.createElement('li');
+    // 
+    const val = document.body.querySelector('#name').value;
+    li.innerHTML = val;
+
+    const type = document.body.querySelector('#type').value;
+    let color = '';
+    li.setAttribute('data-type', type);
+    switch(type) {
+        case 'drinks':
+          color = 'red';
+          break;
+        case 'bakery':
+          color = 'green'
+          break;
+        case 'canned':
+          color = 'brown';
+          break;
+        case 'dairy':
+          color = 'blue';
+          break;
+        case 'frozen':
+          color = 'black';
+          break;
+        case 'meats':
+          color = 'orange';
+          break;
+        case 'produce':
+          color = 'yellow'
+          break;
+        case 'toiletries':
+          color = 'pink'
+          break;
+        case 'other':
+          color = 'purple';
+          break;
+      }
+      li.style.backgroundColor = color;
+
+
+    document.body.querySelector('#shopping-list').append(li);
+  });
+});
